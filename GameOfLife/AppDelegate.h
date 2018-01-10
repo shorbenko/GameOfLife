65ab436ab4ee94d922fee1f5b708c4f22bd88dfd
Serhii Horbenko <shorben@softserveinc.com>
2018-01-10 20:32:54 +0200

    Initial Commit

---
+++ GameOfLife/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GameOfLife
+//
+//  Created by Serhii Horbenko on 1/10/18.
+//  Copyright © 2018 Serhii Horbenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GameOfLife/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GameOfLife
+//
+//  Created by Serhii Horbenko on 1/10/18.
+//  Copyright © 2018 Serhii Horbenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
